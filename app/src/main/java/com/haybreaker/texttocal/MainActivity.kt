9f57f6c52ed3fdf6070dc5ae98c1d4be9393d2db
Jesse Hayward <jrhayward@haybreaker.com>
2020-10-23 22:10:06 +1100

    Frontend Built and Designed, Backend ready for Google API Implementation

--- app/src/main/java/com/haybreaker/texttocal/MainActivity.kt
+++ app/src/main/java/com/haybreaker/texttocal/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.haybreaker.texttocal
 
 import android.Manifest
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+import java.io.InputStream
+import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,21 +46,44 @@ class MainActivity : AppCompatActivity() {
             requestPermissions(arrayOf(Manifest.permission.READ_SMS), 1111)
         }
         else{
-            //Now that permissions are set correctly go through and run following functions
-
-            //Grab all relevant messages
-            var relevantMessages: MutableList<String> = readMessages()
-            //Put messages through algorithm to grab array of shifts
-            var shiftData: MutableList<String> = calculateShifts(relevantMessages)
-            //Import all calculated data into calender
-            importToCal(shiftData)
+            //Permissions set so now we wait for import to be initiated by user
+            importButton.setOnClickListener(){
+                //Now that permissions are set correctly go through and run following functions
+                //Grab all relevant messages
+                var relevantMessages: MutableList<String> = readMessages()
+                //Put messages through algorithm to grab array of shifts
+                var shiftData: MutableList<String> = calculateShifts(relevantMessages)
+                //Import all calculated data into calender
+                importToCal(shiftData)
+            }
         }
 
+        //Outside of permissions and other functions setup debugging mode and processes.
+        textToCalImage.setOnLongClickListener(){
+            Toast.makeText(this,"Debugging functions coming shortly",Toast.LENGTH_SHORT).show()
+            return@setOnLongClickListener  true
+
+        }
     }
-    private fun calendarSetup(){
 
+    private fun calendarSetup() {
+        //Initialise initial variables to connect with google API
+        /*var applicationName = "textToCal"
+        var jsonFactory = JacksonFactory.getDefaultInstance()
+        var tokensDirectoryPath = "tokens"
 
+        //Further variables delcared however if changes made need to delete tokens
+        var SCOPES: List<String> = Collections.singletonList(CalendarScopes.CALENDAR_READONLY)
+        var credentialsFilePath = "/credentials.json"
 
+        //Finalize other parts, more description to come
+        var Credential = getCredentials(var NetHttpTransport HTTP_TRANSPORT){
+            // Load client secrets.
+            InputStream in = CalendarQuickstart .class.getResourceAsStream(CREDENTIALS_FILE_PATH)
+        }
+
+        var clientSecrets: GoogleClientSecrets =
+            GoogleClientSecrets.load(jsonFactory, InputStreamReader(in))*/
     }
 
     private fun readMessages(): MutableList<String> {
@@ -71,7 +117,8 @@ class MainActivity : AppCompatActivity() {
             //Time to write a comment :)
             //Clear the message and then display all messages for debugging
 
-            lastSyncMessage.text = ""
+            //LEGACY CODE ===== No longer using lastSyncMessage as debugging code
+            //lastSyncMessage.text = ""
 
             for (item in messagesList) {
                 //Legacy testing code
@@ -132,6 +179,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun importToCal(shiftData: MutableList<String>){
 
+        /*var cr: ContentResolver = this.contentResolver
+        var cv: ContentValues = ContentValues()
+
+        cv.put(CalendarContract.Events.TITLE,"Work - Telstra Knox")
+        cv.put(CalendarContract.Events.DESCRIPTION, "Working at Telstra Knox Today")
+        cv.put(CalendarContract.Events.EVENT_LOCATION, "Telstra Knox City")
+        cv.put(CalendarContract.Events.DTSTART, Calendar.getInstance().timeInMillis)
+        cv.put(CalendarContract.Events.DTEND, Calendar.getInstance().timeInMillis + 60*60*1000)
+        cv.put(CalendarContract.Events.CALENDAR_ID, 1)
+        cv.put(CalendarContract.Events.EVENT_TIMEZONE, Calendar.getInstance().timeZone.id)
+
+        var uri: Uri = cr.insert(CalendarContract.Events.CONTENT_URI, cv)
+
+        Toast.makeText(this, "Event is added", Toast.LENGTH_SHORT).show()*/
     }
+
     //Close off all functions by here
 }
